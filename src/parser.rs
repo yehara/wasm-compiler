65a1e444efc8ed1ca292bbb2d145c3e35a462b67
yehara <yoshinori.ehara@gmail.com>
2022-08-02 10:38:12 +0900

    トークナイザで、アルファベット1文字の変数名に対応。Rust のユニットテストも追加。

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,9 @@
 #[derive(PartialEq)]
+#[derive(Debug)]
 pub enum Token<'a> {
     Num(i32),
     Reserved(&'a str),
+    Ident(&'a str),
 }
 
 pub struct TokenIterator<'a> {
@@ -64,6 +66,14 @@ impl<'a> Iterator for TokenIterator<'a> {
             self.s = self.s.split_at(1).1;
             return Some(Token::Reserved("/"));
         }
+        match self.s.chars().next() {
+            Some('a'..='z') => {
+                let (ident, rest) = self.s.split_at(1);
+                self.s = rest;
+                return Some(Token::Ident(ident));
+            }
+            _ => (),
+        }
         let (digit_s, remain_s) = split_digit(self.s);
         if !digit_s.is_empty() {
             self.s = remain_s;
@@ -77,4 +87,36 @@ impl<'a> Iterator for TokenIterator<'a> {
 fn split_digit(s: &str) -> (&str, &str) {
     let first_non_num_idx = s.find(|c| !char::is_numeric(c)).unwrap_or(s.len());
     s.split_at(first_non_num_idx)
+}
+
+
+#[test]
+fn test() {
+    let mut it = TokenIterator { s: "(18-2)/2*4+a" }.peekable();
+    assert_eq!(it.next(), Some(Token::Reserved("(")));
+    assert_eq!(it.next(), Some(Token::Num(18)));
+    assert_eq!(it.next(), Some(Token::Reserved("-")));
+    assert_eq!(it.next(), Some(Token::Num(2)));
+    assert_eq!(it.next(), Some(Token::Reserved(")")));
+    assert_eq!(it.next(), Some(Token::Reserved("/")));
+    assert_eq!(it.next(), Some(Token::Num(2)));
+    assert_eq!(it.next(), Some(Token::Reserved("*")));
+    assert_eq!(it.next(), Some(Token::Num(4)));
+    assert_eq!(it.next(), Some(Token::Reserved("+")));
+    assert_eq!(it.next(), Some(Token::Ident("a")));
+    assert_eq!(it.next(), None);
+}
+
+#[test]
+fn test_rel() {
+    let mut it = TokenIterator { s: "1<2<=3>=2>1" }.peekable();
+    assert_eq!(it.next(), Some(Token::Num(1)));
+    assert_eq!(it.next(), Some(Token::Reserved("<")));
+    assert_eq!(it.next(), Some(Token::Num(2)));
+    assert_eq!(it.next(), Some(Token::Reserved("<=")));
+    assert_eq!(it.next(), Some(Token::Num(3)));
+    assert_eq!(it.next(), Some(Token::Reserved(">=")));
+    assert_eq!(it.next(), Some(Token::Num(2)));
+    assert_eq!(it.next(), Some(Token::Reserved(">")));
+    assert_eq!(it.next(), Some(Token::Num(1)));
 }
