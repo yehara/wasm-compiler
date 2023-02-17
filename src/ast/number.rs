e8f5eac73908be42be0e4d506939e0d74fe868d2
yehara <yoshinori.ehara@gmail.com>
2023-02-17 23:12:53 +0900

    バイナリ出力の整数リテラルで LEB128 を使うように。これまでは1バイトで表せる数字しか使えなかったが、i32 の範囲の数字を使えるように。 ただし負数の場合 - は数値リテラルに含まず単項演算子とみなしているので、-2147483648 はパースできない(2147483648 は i32 の範囲を超えるので)

--- src/ast/number.rs
+++ src/ast/number.rs
@@ -1,5 +1,6 @@
 use std::io::Write;
 use crate::ast::{AstNode, Function, Module, WasmWriter, WatWriter};
+use crate::ast::leb128::i32_to_leb128;
 
 pub struct Number {
     value: i32
@@ -15,7 +16,7 @@ impl WatWriter for Number {
 impl WasmWriter for Number {
     fn write_wasm(&self, _module: Option<&Module>, _function: Option<&Function>, write: &mut dyn Write) -> std::io::Result<()> {
         write.write(&vec![0x41])?;             // i32.const
-        write.write(&vec![self.value as u8])?; // i32 literal
+        write.write(&i32_to_leb128(self.value))?; // i32 literal
         Ok(())
     }
 }
